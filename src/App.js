c9caaa354789445825edffafa9b0f051966abfc9
ttt2205 <thanhtrung22052004@gmail.com>
2024-10-09 15:08:03 +0700

    backup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import LoginPage from './routes/LoginPage';
+import NoPage from './routes/NoPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<LoginPage />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
